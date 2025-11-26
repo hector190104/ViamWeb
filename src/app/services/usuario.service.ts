49a77c651fa9ae4a2adb2f63e82d0692a48a261c
hector190104 <cesarxm19@gmail.com>
2025-11-26 01:57:15 -0600

    third committttTTpo

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from '../../environments/environments'; // 👈 importa environment
+import { environment } from '../../environments/environment'; // 👈 importa environment
 
 @Injectable({ providedIn: 'root' })
 export class UsuarioService {
