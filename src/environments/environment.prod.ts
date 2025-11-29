7ff1fc38b09c7768d5daf1ab8a68091b3d486233
hector190104 <cesarxm19@gmail.com>
2025-11-28 18:48:53 -0600

    Forzando un deploy23

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://viamapi-production.up.railway.app/api' // 👈 reemplaza con tu URL pública de Railway
+  apiUrl: 'https://viamapi-production.up.railway.app/api'
 };
